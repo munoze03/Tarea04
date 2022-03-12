ec3a253d3d3dc43b8f5d62a292ce90eb3b087d22
Kike <Kike@DESKTOP-HTURJ4H>
2022-03-12 18:13:32 +0100

    3- Introducir el método operativa_cuenta, que englobe las sentencias de

--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -5,9 +5,7 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = operativo_Cuenta();
 
         try {
             cuenta1.retirar(2300);
@@ -21,4 +19,13 @@ public class Main {
             System.out.print("Fallo al ingresar");
         }
     }
+
+	private static CCuenta operativo_Cuenta() {
+		CCuenta cuenta1;
+		double saldoActual;
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+		return cuenta1;
+	}
 }
